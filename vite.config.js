ab8f6fe0dd6991e812e0b4d8768f8b927f8f04c6
aladin002dz <mahfoudh.arous@gmail.com>
2025-11-24 22:10:13 +0100

    update deployment github actions

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || '/testing-ws-algeria2.0-2025/',
+  base: '/testing-ws-algeria2.0-2025/',
 })
